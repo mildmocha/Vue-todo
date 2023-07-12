d9f41e208c74833b714c34b79f95ef5c47bb94ab
mildmocha <128768439+mildmocha@users.noreply.github.com>
2023-07-12 19:02:51 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,56 @@
 <script setup>
-import { ref, watch, onMounted } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 
-const todos = ref([]);
-const name = ref('');
+  const todos = ref([]);
+  const name = ref('');
 
-const input_content = ref('')//입력값
-const input_category = ref(null);
+  const input_content = ref(''); //입력내용
+  const input_category = ref(null);
 
-const addTodo = () =>{
-  if (input_content.value.trim() === '' || input_category.value === null ){ 
-
-  return
-
-}
-  todos.value.push({
-       content: input_content.value,
-       category: input_category.value,
+  //입력값을 todos배열에 넣는 함수
+  const addTodo = () => {
+    if (input_content.value.trim() === '' || input_category.value === null ){ 
+      return
+    }
+    todos.value.push({
+      content: input_content.value,
+      category: input_category.value,
       createAt: new Date().getTime(),
       done:false,
       editable:false
-     })
+    })
     //입력 후 초기화
     input_content.value = '';
     input_category.value= null;
+    console.log('todos: ',todos)
   }
 
+  //시간순으로 순서 정리( b.createAt - a.createAt :새로운 todo가 위에 보임)
+  const todos_asc = computed(()=> todos.value.sort((a,b) => { 
+    return b.createAt - a.createAt } ))
+
+  //삭제 함수
+  const remoeveTodo = (todo) => {
+    todos.value = todos.value.filter((t)=> t !== todo );
+  } 
 
 
+  //이름입력하는 것을 인지하고 로컬스토리지에 저장,업데이트
+  watch(name, (newVal) => {
+    localStorage.setItem('name',newVal)
+  })
 
+  //todos가 변하는 것을 인지하고 로컬스토리지 저장,업데이트
+  watch(todos, (newVal) => {
+    localStorage.setItem('todos',JSON.stringify(newVal))
+  },{ deep:true } )  //deep -프로퍼티나, 하위 뎁스값도 감시
 
-    //이름입력하는 것을 인지하고 로컬스토리지에 저장
+  //새로열었을때 로컬스토리지에서 불러오기(없을때는 비워놈)
+  onMounted(()=>{
+    name.value = localStorage.getItem('name') || '';
+    todos.value = JSON.parse(localStorage.getItem('todos') || [])
+  })
 
-watch(name, (newName) => {
-  localStorage.setItem('name', newName)
-})
-//todo 변화 감지 로컬스토리지 업데이트
-watch(todos, (newVal)=>{
-  localStorage.setItem('todos',JSON.stringify(newVal))
-},{deep:true})
-
-
-//로컬스토리지 불러오기
-onMounted(() => {
-  name.value = localStorage.getItem('name') || '';
-})
 
 </script>
 
@@ -51,55 +58,58 @@ onMounted(() => {
   <main class="app">
     <section class="greeting">
       <h1 class="title">
-        whatsup
+        What's Up 
         <input type="text" placeholder="name here" v-model="name">
-
-      </h1>
-
+      </h1>      
     </section>
 
-    <section class="create-todo" v-on:submit.prevent="addTodo">
-      <h2> create-todo</h2>
-      <form id="new-todo-form">
-        <h4> whats on ur todo list </h4>
-        <input type="text" id="content" placeholder="e.g 포트폴리오 마무리"
-        v-model="input_content"
+    <section class="create-todo">
+      <h2>CREATE A TODO</h2>
+      <form id="new-todo-form" v-on:submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input 
+          type="text" 
+          id="content" 
+          placeholder="e.g portfolio finish"
+          v-model="input_content"
         >
-        {{ input_content }}
+        <!-- {{ input_content  }} -->
         <div class="options">
           <label>
-
-            <input type="radio" name="category" id="category1" value="business" v-model="input_category">
+            <input type="radio" name="category" id="categogy1" value="business" v-model="input_category">
             <span class="bubble business"></span>
             <div>business</div>
           </label>
           <label>
-            <input type="radio" name="category" id="category2" value="personal" v-model="input_category">
+            <input type="radio" name="category" id="categogy2" value="personal" v-model="input_category">
             <span class="bubble personal"></span>
             <div>personal</div>
-          </label>
+          </label>          
         </div>
-    <!--    {{ input_category }}-->
-
+        <!-- {{ input_category }} -->
         <input type="submit" value="Add todo">
-
       </form>
     </section>
 
     <section class="todo-list">
-      <div class="todo-item">
-        <div>
-          <label for="">
-            <input type="checkBox">
-            <span class="personal"></span>
+      <h3>TODO LIST</h3>     
+      <div class="list" id="todo-list">
+        
+        <div v-for="todo in todos_asc" v-bind:class ="`todo-item ${todo.done && 'done'}`"> 
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
           </label>
+
           <div class="todo-content">
-            <input type="text" name="" id="" v-model="input_content">
+            <input type="text" v-model="todo.content">
           </div>
           <div class="actions">
-            <button class="delete" @click=""></button>
-          </div> 
-        </div>
+            <button class="delete" @click="remoeveTodo(todo)">
+              Delete
+            </button>
+          </div>
+        </div>      
       </div>
     </section>
   </main>
